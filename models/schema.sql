5963a6cbe4965accf969967a87f3e1c959f42eb7
ngiridhar1 <50300536+ngiridhar1@users.noreply.github.com>
2019-09-12 10:52:55 -0400

    Adding schema.sql

--- models/schema.sql
+++ models/schema.sql
@@ -1,5 +1,17 @@
-DROP DATABASE IF EXISTS exampledb;
-CREATE DATABASE exampledb;
+DROP DATABASE IF EXISTS nutritionDB;
+CREATE database nutritionDB;
 
-DROP DATABASE IF EXISTS testdb;
-CREATE DATABASE testdb;
+USE nutritionDB;
+
+CREATE TABLE getfit (
+  userid   VARCHAR(10) NOT NULL,
+  password VARCHAR(15) NULL,
+  height   INT NOT NULL,
+  weight   INT NOT NULL,
+  age      INT NOT NULL,
+  gender   VARCHAR(1) NOT NULL,
+  indate   DATE NOT NULL,
+  calories INT NOT NULL,
+  PRIMARY KEY (userid)
+ );
+  
